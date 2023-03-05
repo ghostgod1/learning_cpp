3d3c366420dcc6d16a1ec53e650a925da0639854
ghostgod1 <73012930+ghostgod1@users.noreply.github.com>
2023-03-05 23:20:47 +0530

    binary search

--- Arrays/08_Binary_Search.cpp
+++ Arrays/08_Binary_Search.cpp
@@ -2,5 +2,49 @@
 using namespace std;
 int main()
 {
-    
+    int A[100],len,mid,start,end,key;
+
+    cout<<"Enter length of array : ";
+    cin>>len;
+
+    start = 0;
+    end = len-1;
+
+    cout<<"Enter elements of array :"<<endl;
+    for(int i=0;i<len;i++)
+    {
+        cout<<"A["<<i<<"] = ";
+        cin>>A[i];
+    }
+
+    cout<<"Array = [";
+    for(int i=0;i<len;i++)
+    {
+        cout<<A[i]<<",";
+    }
+    cout<<"]"<<endl;
+
+    cout<<"Enter key to be searched :";
+    cin>>key;
+
+
+    while(start<=end)
+    {
+        mid = (start + end)/2;
+
+        if(key == A[mid])
+        {
+            cout<<"key found at "<<mid;
+            return 0;
+        }
+        else if(key<A[mid])
+        {
+            end = mid-1;
+        }
+        else if(key>A[mid])
+        {
+            start = mid + 1;
+        }
+    }
+    cout<<"key not found.";
 }
